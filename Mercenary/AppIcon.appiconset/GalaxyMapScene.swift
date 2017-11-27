e1e14bb9b69b48616307c91ee49f4ab3d344c828
Will Egesdal <william.egesdal@gmail.com>
2017-11-26 19:36:55 -1000

    connected scene and swift file

--- Mercenary/AppIcon.appiconset/GalaxyMapScene.swift
+++ Mercenary/AppIcon.appiconset/GalaxyMapScene.swift
@@ -9,7 +9,28 @@
 import Foundation
 import SpriteKit
 class GalaxyMapScene:SKScene {
+    var map: SKTileMapNode!
+    let planet = SKSpriteNode(imageNamed:"DemeterP.gif")
+    override func didMove(to view: SKView) {
+        loadSceneNodes()
+        //Add Planet
+        planet.name = "planet"
+        planet.texture!.filteringMode = .nearest
+        planet.setScale(5.0)
+        planet.position = (CGPoint(x:0, y:0))
+        planet.zPosition = 100
+        addChild(planet)
+    }
 
+    func loadSceneNodes() {
+
+        guard let map = childNode(withName: "map")
+            as? SKTileMapNode else {
+                fatalError("Background node not loaded")
+        }
+        self.map = map
+        
+    }
 }
 
 
