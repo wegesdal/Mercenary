9064fbf8580b42e60daa4650ebaed59d82df565c
Will Egesdal <william.egesdal@gmail.com>
2017-11-26 23:11:30 -1000

    making progress on galaxy map. all 60 systems are included

--- Mercenary/AppIcon.appiconset/GalaxyMapScene.swift
+++ Mercenary/AppIcon.appiconset/GalaxyMapScene.swift
@@ -16,7 +16,7 @@ class GalaxyMapScene:SKScene {
         var mapX = 0
         var mapY = 0
         var truncatedPlanetNames: [String] = []
-        let planetNames = ["AbeonaP.gif", "AditiP.gif", "AegirP.gif", "AntiP.gif", "BadbP.gif", "BalderP.gif", "BelenusP.gif", "BuriP.gif", "CamulosP.gif", "CandraP.gif", "CipactliP.gif", "DaphneP.gif", "DebwenP.gif", "DemeterP.gif", "EAlomP.gif", "EkChuahP.gif", "FaivarongoP.gif", "FujinP.gif", "GarudaS.gif", "GaunabP.gif", "GerraP.gif", "GukumatzS.gif", "HastseyaltiP.gif", "HeimdallS.gif", "HoursP.gif", "HuangTiS.gif", "IgalilikP.gif", "IndraP.gif", "IuturnaP.gif", "JabruS.gif", "JokinamP.gif", "JunoS.gif", "KartaP.gif", "KujuS.gif", "KybeleP.gif", "LeukotheaP.gif"]
+        let planetNames = ["AbeonaP.gif", "AditiP.gif", "AegirP.gif", "AntiP.gif", "BadbP.gif", "BalderP.gif", "BuriP.gif", "CamulosP.gif", "CandraP.gif", "CipactliP.gif", "DaphneP.gif", "DebwenP.gif", "DemeterP.gif", "EAlomP.gif", "EkChuahP.gif", "FaivarongoP.gif", "FujinP.gif", "GarudaS.gif", "GaunabP.gif", "GerraP.gif", "GukumatzS.gif", "HastseyaltiP.gif", "HeimdallS.gif", "HoursP.gif", "HuangTiS.gif", "IgalilikP.gif", "IndraP.gif", "IuturnaP.gif", "JabruS.gif", "JokinamP.gif", "JunoS.gif", "KartaP.gif", "KujuS.gif", "KybeleP.gif", "LeukotheaP.gif", "LiluriP.gif", "LomoP.gif", "MamlamboP.gif", "NingiramaP.gif", "OsandeP.gif", "PoenimusP.gif", "QeskinaquS.gif", "RanP.gif", "SganaP.gif", "SisyphosP.gif", "SogblenP.gif", "TinP.gif", "TuleS.gif", "UsasP.gif", "UsinsS.gif", "WajetP.gif", "WepwawetP.gif", "WusquusS.gif", "XolotlNanahuatlP.gif", "YamadutiP.gif", "YinamtilanP.gif", "YumCimilP.gif", "ZababaP.gif", "ZipaknaP.gif", "ZurvanP.gif"]
         let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: planetNames) as! [String]
         for planetName in shuffled {
             let planet = SKSpriteNode(imageNamed: planetName)
@@ -37,7 +37,7 @@ class GalaxyMapScene:SKScene {
             planet.name = truncatedWithoutLetter
             addChild(planet)
             mapX += 1
-            if mapX > 5 {
+            if mapX > 9 {
                 mapY += 1
                 mapX = 0
             }
@@ -57,11 +57,23 @@ class GalaxyMapScene:SKScene {
     
     func sceneTouched(touchLocation:CGPoint) {
         print("touched")
+        childNode(withName: "thumb")?.removeFromParent()
         for node in children {
             if node.name != "map" {
                 if node.contains(touchLocation) {
                     run(SKAction.playSoundFileNamed("sfx_menu_select4.wav", waitForCompletion: false))
-                    print(node.name ?? "planet")
+                    if let portraitFilename = node.name {
+                        
+                        let portrait = portraitFilename + ".jpg"
+                        print(portrait)
+                        let thumb = SKSpriteNode(imageNamed: portrait)
+                        thumb.zPosition = -100
+                        thumb.name = "thumb"
+                        addChild(thumb)
+                    } else {
+                        print("There are no portraits with that name.")
+                    }
+
                 }
             }
         }
