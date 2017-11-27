c4cbe3f1b19c492bc66e434eecdedad3f1c8c1e1
Will Egesdal <william.egesdal@gmail.com>
2017-11-26 21:04:12 -1000

    truncated list of names refactored variable names

--- Mercenary/AppIcon.appiconset/GalaxyMapScene.swift
+++ Mercenary/AppIcon.appiconset/GalaxyMapScene.swift
@@ -15,23 +15,24 @@ class GalaxyMapScene:SKScene {
         loadSceneNodes()
         var mapX = 0
         var mapY = 0
-        var bar: [String] = []
+        var truncatedPlanetNames: [String] = []
         let planetNames = ["AbeonaP.gif", "AditiP.gif", "AegirP.gif", "AntiP.gif", "BadbP.gif", "BalderP.gif", "BelenusP.gif", "BuriP.gif", "CamulosP.gif", "CandraP.gif", "CipactliP.gif", "DaphneP.gif", "DebwenP.gif", "DemeterP.gif", "EAlomP.gif", "EkChuahP.gif", "FaivarongoP.gif", "FujinP.gif", "GarudaS.gif", "GaunabP.gif", "GerraP.gif", "GukumatzS.gif", "HastseyaltiP.gif", "HeimdallS.gif", "HoursP.gif", "HuangTiS.gif", "IgalilikP.gif", "IndraP.gif", "IuturnaP.gif", "JabruS.gif", "JokinamP.gif", "JunoS.gif", "KartaP.gif", "KujuS.gif", "KybeleP.gif", "LeukotheaP.gif"]
         let shuffled = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: planetNames) as! [String]
         for planetName in shuffled {
             let planet = SKSpriteNode(imageNamed: planetName)
         //Add Planets
-            planet.name = planetName
+            
             planet.texture!.filteringMode = .nearest
             planet.setScale(1.0)
             planet.alpha = 0.80
             let planetPosition = map.centerOfTile(atColumn: mapX, row: mapY)
             planet.position = planetPosition
             planet.zPosition = 100
-            var myStringArr1 = planetName.components(separatedBy: ".")
-            let foo: String = myStringArr1 [0]
-            let truncated = String(foo[..<foo.index(before: foo.endIndex)])
-            bar.append(truncated)
+            var truncateAtDotArr = planetName.components(separatedBy: ".")
+            let truncatedWithLetter: String = truncateAtDotArr [0]
+            let truncatedWithoutLetter = String(truncatedWithLetter[..<truncatedWithLetter.index(before: truncatedWithLetter.endIndex)])
+            truncatedPlanetNames.append(truncatedWithoutLetter)
+            planet.name = truncatedWithoutLetter
             addChild(planet)
             mapX += 1
             if mapX > 5 {
@@ -39,7 +40,7 @@ class GalaxyMapScene:SKScene {
                 mapX = 0
             }
         }
-        print(bar)
+        print(truncatedPlanetNames)
     }
 
     func loadSceneNodes() {
